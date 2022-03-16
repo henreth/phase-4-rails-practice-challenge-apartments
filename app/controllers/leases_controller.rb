c7e36103c397e3df6cae9f77e666974b0081ce03
Henry Miller <mail.henrym+git@gmail.com>
2022-03-16 15:02:13 -0400

    commit tree 879c89295f39b2c5e2c77ed25e20f89588fbad0d

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,29 @@
 class LeasesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def create
+        lease = Lease.create(lease_params)
+        render json: lease
+    end
+
+    def destroy 
+        lease = find_lease
+        lease.destroy
+        head :no_content
+    end
+  
+    private
+
+    def find_lease
+        Lease.find_by(id: params[:id])
+    end
+  
+    def render_not_found_response
+      render json: { error: "Lease not found" }, status: :not_found
+    end
+
+    def lease_params
+        params.permit(:rent, :tenant_id, :apartment_id)
+    end
+
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -7,14 +7,39 @@ class TenantsController < ApplicationController
     end
   
     def show
-      tenant = Tenant.find(params[:id])
+      tenant = find_tenant
       render json: tenant
+    end
+
+    def create
+        tenant = Tenant.create(tenant_params)
+        render json: tenant
+    end
+
+    def update
+        tenant = find_tenant
+        tenant.update(tenant_params)
+        render json: tenant
+    end
+
+    def destroy 
+        tenant = find_tenant
+        tenant.destroy
+        head :no_content
     end
   
     private
+
+    def find_tenant
+        Tenant.find_by(id: params[:id])
+    end
   
     def render_not_found_response
       render json: { error: "Tenant not found" }, status: :not_found
+    end
+
+    def tenant_params
+        params.permit(:name, :age)
     end
   
 end
